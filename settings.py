5520a072e045e6a6702f46df9211ca714a0dfe02
Patrick Connolly <patrick.c.connolly@gmail.com>
2019-02-21 16:17:21 -0500

    Added support for a whitelisto of event name sting matches. Added config docs. [Closes #5]

--- settings.py
+++ settings.py
@@ -5,4 +5,5 @@ load_dotenv(dotenv_path='.env')
 
 MEETUP_API_KEY = os.getenv('MEETUP_API_KEY')
 MEETUP_GROUP_SLUG = os.getenv('MEETUP_GROUP_SLUG')
+MEETUP_EVENT_NAME_WHITELIST = os.getenv('MEETUP_EVENT_NAME_WHITELIST', "")
 PORT = os.getenv('PORT', 5000)
